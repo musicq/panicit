06b746fa68d6b57fa2a5460f1f11a2461d8a17a7
Liang Kui <kui.liang@shopee.com>
2023-04-18 11:56:49 +0800

    feat: nest error cause
    - fix: show object when error or cause is object

--- tests/panic.test.ts
+++ tests/panic.test.ts
@@ -5,25 +5,58 @@ const mockProcessExit = vi
   .spyOn(process, 'exit')
   .mockImplementation((() => {}) as any)
 const mockConsoleError = vi.spyOn(console, 'error').mockImplementation(() => {})
+const mockConsoleGroup = vi.spyOn(console, 'group').mockImplementation(() => {})
 
 afterEach(() => {
   vi.clearAllMocks()
 })
 
 test('panic', () => {
-  expect(() => panic('error message')).toThrowError(/error message/)
+  expect(() => panic('error message')).toThrowErrorMatchingInlineSnapshot(
+    '"error message"'
+  )
   expect(mockProcessExit).toHaveBeenCalledOnce()
   expect(mockProcessExit).toBeCalledWith(1)
   expect(mockConsoleError).toHaveBeenCalledOnce()
-  expect(mockConsoleError).toBeCalledWith('error message', '')
+  expect(mockConsoleError).toBeCalledWith('[panic]', 'error message')
 })
 
 test('panic with error code = 2 and error cause', () => {
   expect(() =>
     panic('error message', { exitCode: 2, cause: 'error cause' })
-  ).toThrow(/error message/)
+  ).toThrowErrorMatchingInlineSnapshot('"error message"')
   expect(mockProcessExit).toHaveBeenCalledOnce()
   expect(mockProcessExit).toBeCalledWith(2)
+  expect(mockConsoleGroup).toBeCalledTimes(1)
+  expect(mockConsoleGroup).toHaveBeenCalledWith('[panic]', 'error message')
   expect(mockConsoleError).toBeCalledTimes(1)
-  expect(mockConsoleError).toBeCalledWith('error message', '\n[Cause] error cause')
+  expect(mockConsoleError).toBeCalledWith('[Cause]', 'error cause')
+})
+
+test('error and cause are objects', () => {
+  expect(() =>
+    panic({ error: 'some error' }, { cause: { reason: 'some cause' } })
+  ).toThrowErrorMatchingInlineSnapshot('"[object Object]"')
+  expect(mockProcessExit).toHaveBeenCalledOnce()
+  expect(mockProcessExit).toBeCalledWith(1)
+  expect(mockConsoleGroup).toBeCalledTimes(1)
+  expect(mockConsoleGroup).toHaveBeenCalledWith(
+    '[panic]',
+    expect.objectContaining({ error: 'some error' })
+  )
+  expect(mockConsoleError).toBeCalledTimes(1)
+  expect(mockConsoleError).toHaveBeenCalledWith(
+    '[Cause]',
+    expect.objectContaining({ reason: 'some cause' })
+  )
+})
+
+test('silent output', () => {
+  expect(() =>
+    panic({ error: 'some error' }, { cause: 'some cause', silent: true })
+  ).toThrowErrorMatchingInlineSnapshot('"[object Object]"')
+  expect(mockProcessExit).toHaveBeenCalledOnce()
+  expect(mockProcessExit).toBeCalledWith(1)
+  expect(mockConsoleGroup).not.toBeCalled()
+  expect(mockConsoleError).not.toBeCalled()
 })
